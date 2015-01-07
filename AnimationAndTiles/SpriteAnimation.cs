b1352605360df41494cbb616b919dfcb509e8520
Scr4py <kevinholst@gmx.net>
2015-01-07 15:12:13 +0100

    SpriteFrames und SpriteAnimation erstellt

---
+++ AnimationAndTiles/SpriteAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace AnimationAndTiles
+{
+    class SpriteAnimation
+    {
+
+
+
+        private void LoadFrames(string path)
+        {
+            XmlReader
+        }
+    }
+}
